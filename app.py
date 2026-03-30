7086749d442f346e489b9aab281389d29701c18c
pugal000 <pugalkiller0@gmail.com>
2026-03-30 17:41:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -106,7 +106,7 @@ def check():
     res = requests.get(url, headers=headers)
 
     if code in res.text:
-        BOT_TOKEN = os.environ.get("BOT_TOKEN")
+        BOT_TOKEN = "MTQ4NzUyMjc5NTExMzE1Njc2OQ.GnswoZ.QfQm64u7QLfmjAl6vKCD9DwAzTDW85aVo_U4hU"
         GUILD_ID = "1484761131657723934"
         ROLE_ID = "1487321755151503500"
 
